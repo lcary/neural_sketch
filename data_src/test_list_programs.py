368207a869a8396b59f7793fcae172bc214bb90c
Luc Cary <luc.cary@gmail.com>
2019-08-13 10:57:03 -0400

    run black to clean up the code

--- data_src/test_list_programs.py
+++ data_src/test_list_programs.py
@@ -1,35 +1,36 @@
 import sys
 import os
-sys.path.append(os.path.abspath('..'))
+
+sys.path.append(os.path.abspath(".."))
 
 import numpy as np
 
-from data_src.dc_program import test_program, generate_IO_examples
+from data_src.dc_program import test_program
 from data_src.dc_program import compile as compile_program
 
 
 def test_sample(sample, program, debug=False):
     if debug:
-        print('input:  ', sample[0])
-        print('expect: ', sample[1])
-        print('actual: ', program.fun(sample[0]))
+        print("input:  ", sample[0])
+        print("expect: ", sample[1])
+        print("actual: ", program.fun(sample[0]))
     assert program.fun(sample[0]) == sample[1]
 
 
 def test_sum_top_index_sorted():
-    source = 'a <- int | b <- [int] | c <- SORT b | d <- TAKE a c | e <- SUM d'
+    source = "a <- int | b <- [int] | c <- SORT b | d <- TAKE a c | e <- SUM d"
     program = test_program(source, N=10)
     sample = ((2, [3, 5, 4, 7, 5]), 7)
     test_sample(sample, program)
 
 
 def test_head_tail():
-    source = 'a <- [int] | b <- HEAD a'
+    source = "a <- [int] | b <- HEAD a"
     program = test_program(source, N=10)
     sample = (([3, 5, 4, 7, 5],), 3)
     test_sample(sample, program)
 
-    source = 'a <- [int] | b <- TAIL a'
+    source = "a <- [int] | b <- TAIL a"
     program = test_program(source, N=10)
     sample = (([3, 5, 4, 7, 5],), [5, 4, 7, 5])
     test_sample(sample, program)
@@ -37,7 +38,7 @@ def test_head_tail():
 
 def test_count_head_in_tail():
     # src1 = 'count (head xs) (tail xs)'
-    source = 'a <- [int] | b <- TAIL a | c <- HEAD a | d <- COUNT c b'
+    source = "a <- [int] | b <- TAIL a | c <- HEAD a | d <- COUNT c b"
     program = generate_interesting_io_examples(source, N=10)
     sample = (([3, 5, 4, 7, 5],), 0)
     test_sample(sample, program)
@@ -52,7 +53,9 @@ def check_variance(samples, min_variance):
     return np.var(outputs) >= min_variance
 
 
-def generate_mixed_length_io_arrays(program, N, V, min_len=1, max_len=10):  # TODO: allow empty lists
+def generate_mixed_length_io_arrays(
+    program, N, V, min_len=1, max_len=10
+):  # TODO: allow empty lists
     """ Given a programs, randomly generates N IO examples.
         using the specified length L for the input arrays. """
     input_types = program.ins
@@ -70,19 +73,28 @@ def generate_mixed_length_io_arrays(program, N, V, min_len=1, max_len=10):  # TO
                 array_size = np.random.randint(min_len, max_len)
                 input_value[a] = list(np.random.randint(minv, maxv, size=array_size))
             else:
-                raise Exception("Unsupported input type " + input_types[a] + " for random input generation")
+                raise Exception(
+                    "Unsupported input type "
+                    + input_types[a]
+                    + " for random input generation"
+                )
         output_value = program.fun(input_value)
         IO.append((input_value, output_value))
-        assert ((program.out == int and output_value <= V) or
-                (program.out == [int] and len(output_value) == 0) or
-                (program.out == [int] and max(output_value) <= V))
+        assert (
+            (program.out == int and output_value <= V)
+            or (program.out == [int] and len(output_value) == 0)
+            or (program.out == [int] and max(output_value) <= V)
+        )
     return IO
 
 
-def generate_interesting_io_examples(source, N=5, V=512, maxv=10, max_io_len=10, min_variance=1.0, timeout=10):
+def generate_interesting_io_examples(
+    source, N=5, V=512, maxv=10, max_io_len=10, min_variance=1.0, timeout=10
+):
     import time
+
     t = time.time()
-    source = source.replace(' | ', '\n')
+    source = source.replace(" | ", "\n")
     program = compile_program(source, V=512, L=maxv)
     interesting = False
     elapsed = time.time() - t
@@ -95,17 +107,19 @@ def generate_interesting_io_examples(source, N=5, V=512, maxv=10, max_io_len=10,
     print(("time:", elapsed))
     print(program)
     if not interesting:
-        print('No interesting samples.')
-    print('samples:')
+        print("No interesting samples.")
+    print("samples:")
     for s in samples:
-        print('    {}'.format(s))
+        print("    {}".format(s))
     return program
 
 
 def test_count_len_in_tail():
     # src2 = 'count (len xs) (tail xs)'
-    source = 'a <- [int] | b <- TAIL a | c <- LEN a | d <- COUNT c b'
-    program = generate_interesting_io_examples(source, N=10, maxv=10, max_io_len=10)  # TODO: change N to 100
+    source = "a <- [int] | b <- TAIL a | c <- LEN a | d <- COUNT c b"
+    program = generate_interesting_io_examples(
+        source, N=10, maxv=10, max_io_len=10
+    )  # TODO: change N to 100
     sample = (([3, 5, 4, 7, 5],), 2)
     test_sample(sample, program)
     sample = (([5, 4, 7, 5],), 1)
@@ -114,7 +128,7 @@ def test_count_len_in_tail():
     test_sample(sample, program)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # test_sum_top_index_sorted()
     # test_head_tail()
     test_count_head_in_tail()
